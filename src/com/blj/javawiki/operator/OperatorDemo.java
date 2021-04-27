69f94b4626330f281abca7b6043f0add890132d7
liangjin.bai <liangjin.bai@megatronix.co>
2021-04-27 15:20:25 +0800

    add OperatorDemo refactor root package name

---
+++ src/com/blj/javawiki/operator/OperatorDemo.java
@@ -0,0 +1,12 @@
+package com.blj.javawiki.operator;
+
+public class OperatorDemo {
+    public static void main(String[] args) {
+        System.out.println("16 >>> 1 =" + (16 >>> 1));
+        System.out.println("((1 << 31)-1) =" + ((1 << 31)-1));
+        System.out.println("Integer.MAX_VALUE = " + Integer.MAX_VALUE);
+
+        System.out.println("(-(1 << 31)) =" + (-(1 << 31)));
+        System.out.println("Integer.MIN_VALUE = " + Integer.MIN_VALUE);
+    }
+}
